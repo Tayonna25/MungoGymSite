26c8af41b91f92668bff9a610ac9fbc28e2451a5
Tayonna25 <botello2@msu.edu>
2025-04-19 10:10:27 -0400

    new  c

---
+++ script.js
@@ -0,0 +1,5 @@
+// Script for handling the CTA button click event
+
+document.getElementById('cta-button').addEventListener('click', function() {
+    alert('Get started with ApexPer4manceTraining today!');
+});
